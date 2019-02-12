80307fff7a03bdb7eb1121422169d1537238b8ed
Muhamamd Abdullah Khalil <muhammadabdullahkhalil@gmail.com>
2019-02-12 22:45:40 +0500

    Student quizzes dynamic

--- app/src/main/java/com/something/mabdullahk/soop/quizzes.java
+++ app/src/main/java/com/something/mabdullahk/soop/quizzes.java
@@ -3,17 +3,38 @@ package com.something.mabdullahk.soop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import com.leo.simplearcloader.ArcConfiguration;
+import com.leo.simplearcloader.SimpleArcDialog;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class quizzes extends AppCompatActivity {
 
 
     RecyclerView recyclerView;
-    List<quizClass> quizzes1;
+    List<quizClass> quizzesList;
+    String studentId;
+    LinearLayout quizzesMainView;
+    ScrollView quizzesScroll;
+    LinearLayout quizzesmsg;
+    SimpleArcDialog mDialog ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +48,97 @@ public class quizzes extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.ic_quiz);
 
 
+        ArcConfiguration configuration = new ArcConfiguration(this);
+        configuration.setLoaderStyle(SimpleArcLoader.STYLE.SIMPLE_ARC);
+        configuration.setText("Fetching Data..");
+        int[] colors = new int[]{R.color.colorlogo1,R.color.colorlogo2,R.color.colorlogo3,R.color.colorlogo4};
+        configuration.setColors(colors);
+        configuration.setAnimationSpeed(SimpleArcLoader.SPEED_MEDIUM);
+
+
+        mDialog = new SimpleArcDialog(this);
+        mDialog.setConfiguration(configuration);
+
+
         recyclerView.setLayoutManager(quizzesGrid);
-        quizzes1 = new ArrayList<>();
+        quizzesList = new ArrayList<>();
+        studentId = (String) getIntent().getStringExtra("studentID");
+        quizzesMainView = (LinearLayout) findViewById(R.id.quizzesMainView);
+        quizzesmsg = (LinearLayout) quizzesMainView.findViewById(R.id.quizzesMessage);
+        quizzesScroll = (ScrollView) findViewById(R.id.quizzesScroll);
 
 
-        quizzes1.add(new quizClass("Exam Term 1 ","Physics","19","100","43","33","50"));
-        quizzes1.add(new quizClass("Quiz 13","English","37","100","21","98","50"));
-        quizzes1.add(new quizClass("Quiz 5","Mathematics","23","100","77","12","50"));
+
+       getquizzes();
 
 
-        quizzesCardAdapter myAdapter = new quizzesCardAdapter(quizzes.this,quizzes1);
-        recyclerView.setAdapter(myAdapter);
+
     }
+
+
+    private void getquizzes(){
+
+        mDialog.show();
+        Map<String, String> params = new HashMap<>();
+        Map<String, String> headers = new HashMap<>();
+        final List<quizClass> temp_quizzesList = new ArrayList<>();
+
+
+        HTTPrequest.placeRequest("https://soop-staging.herokuapp.com/api/v1/parents/children/" + studentId + "/assessments", "Get", params, headers, new HTTPrequest.VolleyCallback() {
+            @Override
+            public void onSuccess(String result) {
+
+                try {
+                    JSONObject students = new JSONObject(result);
+                    Boolean success = students.getBoolean("success");
+
+                    if (success){
+                        JSONArray studentsData = students.getJSONObject("data").getJSONArray("assessments");
+
+                        for(int i=0; i < studentsData.length() ; i++) {
+                            JSONObject quiz = studentsData.getJSONObject(i);
+                            quizzesList.add(new quizClass(
+                                    quiz.getString("id"),
+                                    quiz.getString("date"),
+                                    quiz.getString("title"),
+                                    quiz.getString("subject"),
+                                    quiz.getString("average"),
+                                    quiz.getString("total_marks"),
+                                    quiz.getString("maximum_marks"),
+                                    quiz.getString("marks_obtained")));
+                        }
+
+                        System.out.println(quizzesList+ " is the quiz list.");
+                        if (quizzesList.size() > 0){
+                            mDialog.dismiss();
+                            quizzesMainView.removeView(quizzesmsg);
+                            quizzesCardAdapter myAdapter = new quizzesCardAdapter(quizzes.this,quizzesList);
+                            recyclerView.setAdapter(myAdapter);
+                        } else {
+                            mDialog.dismiss();
+                            quizzesMainView.removeView(quizzesScroll);
+                            quizzesmsg.setVisibility(View.VISIBLE);
+                        }
+
+                    }
+                }catch (JSONException e){
+                    System.out.println("JSON ERROR IN QUIZZES.ajva"+e);
+                }
+            }
+
+            @Override
+            public void onFaliure(String faliure) {
+                System.out.println("it failed i quizzes.java");
+                mDialog.dismiss();
+                quizzesMainView.removeView(quizzesScroll);
+                quizzesmsg.setVisibility(View.VISIBLE);
+            }
+        },this);
+
+    }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
